4840dda5bd52f893143fe25811bbefd7bc98d953
ravanmaxmud <ravanim@code.edu.az>
2023-03-23 14:36:47 +0400

    aded About Viddo Table

---
+++ FoodCorner/Migrations/20230323103616_AboutVido.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodCorner.Migrations
+{
+    /// <inheritdoc />
+    public partial class AboutVido : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vidios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Vidio = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    VidoInFileSystem = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdateAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vidios", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vidios");
+        }
+    }
+}
